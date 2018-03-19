50d8511094d6808a07c89c9d7ab2cded748f4c3e
mengnans <thundersmn@outlook.com>
2018-03-19 22:55:04 +1100

    Show day of the week.

--- ScreenPack/TimerScreen.py
+++ ScreenPack/TimerScreen.py
@@ -1,4 +1,6 @@
 import time
+from datetime import date
+import calendar
 import pygame
 
 from ScreenPack.IScreen import IScreen
@@ -17,15 +19,17 @@ class TimerScreen(IScreen):
         pass
 
     def OnPaint(self):
-        # 这里可以做性能优化，这个优化很重要
+        # TODO: the code here can be optimized
         self.__PaintTime()
         pygame.draw.line(self.canvas, (64, 64, 64), (0, 175), (DataWindow.WindowsSize[0], 175), 1)
         self.__PaintGadget()
 
     def __PaintTime(self):
+        self.font = pygame.font.Font("src\\Font\\Reckoner.ttf", 200)
         _timeContent = time.strftime("%H:%M", time.localtime())
         _renderText = self.font.render(_timeContent, True, (255, 255, 255))
         _recText = _renderText.get_rect()
+        # TODO: try to change the name of attribute, it's hard to understand
         _locationX = (DataWindow.WindowsSize[0] - (_recText[2] - 10)) / 2
         # pygame.draw.rect(self.canvas, (64, 64, 64), (10, 0, _recText[2], _recText[3]), 1)
         # # self.canvas.draw.text(_timeContent, (0, 0), self.font)
@@ -36,6 +40,17 @@ class TimerScreen(IScreen):
         # pygame.draw.line(self.canvas, (64, 64, 64), (0, 175), (DataWindow.WindowsSize[0], 240), 1)
         # pygame.draw.line(self.canvas, (64, 64, 64), (0, 240), (DataWindow.WindowsSize[0], 175), 1)
         self.canvas.blit(self.imgPillDark, (264, 184))
+
+        self.font = pygame.font.Font("src\\Font\\Reckoner.ttf", 58)
+        _date_today = date.today()
+        _weekdayContent = calendar.day_name[_date_today.weekday()][:3]
+        _renderText = self.font.render(_weekdayContent, True, (255, 255, 255))
+        _recText = _renderText.get_rect()
+        # pygame.draw.rect(self.canvas, (64, 64, 64), (10, 0, _recText[2], _recText[3]), 1)
+        # # self.canvas.draw.text(_timeContent, (0, 0), self.font)
+        self.canvas.blit(self.font.render(_weekdayContent, True, (128, 128, 128)), (7, 186))
+        self.canvas.blit(self.font.render(_weekdayContent, True, (255, 255, 255)), (5, 184))
+
 
     def OnKeyDown(self):
         pass
