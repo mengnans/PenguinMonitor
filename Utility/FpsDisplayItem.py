96b41ac9fda711ffbdfa06ed62298a51a796b323
Colin Cheng <rChansey5887@gmail.com>
2018-03-19 13:09:43 +1100

    Before paper prototype

--- Utility/FpsDisplayItem.py
+++ Utility/FpsDisplayItem.py
@@ -7,21 +7,24 @@ from DataPack.DataProgram import DataProgram
 class FpsDisplayItem:
 
     def __init__(self):
-        self.fps = 0
-        self.fpsTick = 0
-        self.second = 0
+        self.__fps = 0
+        self.__fpsTick = 0
+        self.__second = 0
+        self.__lastUpdateTick = 0
 
-        self.canvas = pygame.display.get_surface()
-        self.font = pygame.font.Font("src\\Font\\Inconsolata.otf", 18)
+        self.__canvas = pygame.display.get_surface()
+        self.__font = pygame.font.Font("src\\Font\\Inconsolata.otf", 18)
 
     def Tick(self):
         if DataProgram.IsDebugMode == False:
             return
-        if int(time.time()) == self.second:
-            self.fpsTick = self.fpsTick + 1
+        if int(time.time()) == self.__second:
+            self.__fpsTick = self.__fpsTick + 1
         else:
-            self.second = int(time.time())
-            self.fps = self.fpsTick
-            self.fpsTick = 0
+            _currentTime = time.time()
+            self.__second = int(_currentTime)
+            self.__fps = self.__fpsTick / (_currentTime - self.__lastUpdateTick)
+            self.__lastUpdateTick = _currentTime
+            self.__fpsTick = 0
 
-        self.canvas.blit(self.font.render("FPS: " + str(self.fps), True, (128, 128, 128)), (0, 0))
+        self.__canvas.blit(self.__font.render(str(self.__fps), True, (128, 128, 128)), (2, -2))
