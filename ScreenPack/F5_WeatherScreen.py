b398064440dada2681f6f42e856d2b59c155d3b5
mengnans <thundersmn@outlook.com>
2018-03-27 19:47:26 +1100

    weather

--- ScreenPack/F5_WeatherScreen.py
+++ ScreenPack/F5_WeatherScreen.py
@@ -6,8 +6,8 @@ import pygame
 class WeatherScreen(IScreen):
     def __init__(self):
         self.__canvas = pygame.display.get_surface()
-        _weather = Weather(unit=Unit.CELSIUS)
-        self.__location = _weather.lookup_by_location('melbourne')
+        self.__weather = Weather(unit=Unit.CELSIUS)
+
         self.__font = pygame.font.Font("src/Font/Reckoner.ttf", 540)
         self.__currentWeather = 0
 
@@ -15,8 +15,9 @@ class WeatherScreen(IScreen):
         pass
 
     def OnUpdatePerSecond(self):
+        _location = self.__weather.lookup_by_location('melbourne')
         # self.__currentWeather = self.__location.condition().temp() + '\'C'
-        self.__currentWeather = self.__location.condition().temp() + '\'C'
+        self.__currentWeather = _location.condition().temp() + '\'C'
 
     def OnUpdatePerMinute(self):
         pass
